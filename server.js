a572fbc690233069e93f7b7b08fa927db8fbd555
Gabriel Takarada <gabrieltakarada@gmail.com>
2025-05-28 08:36:38 -0300

    alterando o tamanho maximo para a adição de arquivos

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
+
 
 app.use('/api', produtoRoutes);
 app.use('/api', eventosRoutes);
